8d96b276dcdf2d2ceecdc6a7e0ffb260aea9eec8
Joe D'Alessandro <joe@thejoeshow.net>
2023-05-24 05:30:57 -0400

    initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject calenj "0.1.0"
+  :description "an application/service to collect events from Google Calendar"
+  :url "https://thejoeshow.net"
+  :license {:name "EPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot calenj.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
